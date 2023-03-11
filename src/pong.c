14ef4e4f25bd5a11d832c55250e79a75ff7604fe
Dominica Daxos <dominica@student.21-school.ru>
2023-03-11 18:44:58 +0300

    init commit

--- src/pong.c
+++ src/pong.c
@@ -3,6 +3,19 @@
 #include <stdio.h>
 
 int main() {
-    pritnf("Pong");
+    int MAX_X = 80;
+    int MAX_Y = 25;
+
+    for(int y = 0; y < MAX_Y; y++) {
+       for(int x = 0; x < MAX_X; x++) {
+          if (y == 0 || y == MAX_Y - 1) {
+              printf("-");
+          } else {
+              printf("m");
+          }
+       }
+       printf("\n");
+    }
+     
     return 0;
 }
