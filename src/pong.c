80cb52cc5c7cd913d4f9778d63f9f729bd7cf51d
Dominica Daxos <dominica@student.21-school.ru>
2023-03-11 13:51:33 +0300

    test commit

--- src/pong.c
+++ src/pong.c
@@ -1,5 +1,8 @@
-//I WANT TO PLAY WITH YOU
-//        YOUR FRIEND, AI
+// I WANT TO PLAY WITH YOU
+//         YOUR FRIEND, AI
+#include <stdio.h>
 
-
-
+int main() {
+    pritnf("Pong");
+    return 0;
+}
