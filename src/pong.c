a4b0d714ddb0366d4d97e8f497a991752b458379
Kanesha Torrek <kaneshat@et-i3.kzn.21-school.ru>
2023-03-11 19:55:37 +0300

    implemented ball direction and movement logic

--- src/pong.c
+++ src/pong.c
@@ -1,22 +1,43 @@
 #include <stdio.h>
 
+#define HEIGHT 25
+#define WIDTH 80
+
+
 int render_map(int x_ball, int y_ball, int y_player_1, int y_player_2);
+int move_ball(int x_ball, int direction);
 
 int main() {
-    // while(1)
-    // move_ball(int x_ball, int y_ball);
-    render_map(1, 12, 12, 12);
+    int x_ball = 76;
+    int y_ball = 12;
+    int y_player_1 = 12;
+    int y_player_2 = 12;
+    int direction = 1;
+    char user_input;
+    char checker;
+
+    while(1) {
+        x_ball = move_ball(x_ball, direction);
+
+        if (x_ball == 1 || x_ball == WIDTH-2) direction *= -1;
+
+        int player_turn = scanf("%c%c", &user_input, &checker);
+        if(player_turn == 2 || checker != '\n') {
+            render_map(x_ball, y_ball, y_player_1, y_player_2);
+            printf("x_ball=%d\ndir=%d\n", x_ball, direction);
+        }
+    }
     return 0;
 }
 
 int render_map(int x_ball, int y_ball, int y_player_1, int y_player_2) {
-    const int HEIGHT = 25;
-    const int WIDTH = 80;
+    // const int HEIGHT = 25;
+    // const int WIDTH = 80;
 
     for (int y = 0; y < HEIGHT; y++) {
         for (int x = 0; x < WIDTH; x++) {
             // отрисовка границ верх и низ
-            if ((y == 0 || y == HEIGHT - 1) && x != WIDTH - 1) printf("_");
+            if (y == 0 || y == HEIGHT - 1) printf("_");
 
             // отрисовка мяча
             else if (x == x_ball && y == y_ball)
@@ -37,4 +58,13 @@ int render_map(int x_ball, int y_ball, int y_player_1, int y_player_2) {
         printf("\n");
     }
     return 0;
+}
+
+int move_ball(int x_ball, int direction) {
+    if ( direction > 0 ) {
+        x_ball += 1;
+    } else if (direction < 0){
+        x_ball -= 1;
+    }
+    return x_ball;
 }
